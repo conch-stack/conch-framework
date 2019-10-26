3eff9902416afbbc7325fb9a77dfc31dcf6ba003
zjz6b6 <407091038@qq.com>
2019-10-26 18:26:21 +0800

    init project

---
+++ src/test/java/ltd/beihu/core/framework/BeihuFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package ltd.beihu.core.framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeihuFrameworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
