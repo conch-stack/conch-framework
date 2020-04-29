3cd4a63a120a5d59b16dec6468dd6d6713deafb4
zjz6b6 <407091038@qq.com>
2020-04-29 23:52:30 +0800

    test stream reuse and map null

---
+++ icodeless/src/main/java/ltd/beihu/core/icodeless/TestStream.java
@@ -0,0 +1,68 @@
+package ltd.beihu.core.icodeless;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * @author Adam
+ * @date 2020/4/29
+ */
+public class TestStream {
+
+    public static void testStream() {
+        List<String> testList = new ArrayList<>();
+
+        testList.add("111");
+        testList.add("222");
+        testList.add("eee");
+        testList.add("eeer");
+        testList.add("rrt");
+        testList.add("yyyy");
+
+        Stream<String> stream = testList.stream();
+
+        stream.forEach(System.out::println);
+
+        System.out.println("-----------测试流重复读取---------");
+
+        stream.forEach(System.out::println);
+    }
+
+    public static void testMap1() {
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("1", 1);
+        map.put("2", 2);
+        map.put("3", 3);
+        Integer integer = map.computeIfAbsent("3", key -> new Integer(4));//key存在返回value
+        Integer integer1 = map.computeIfAbsent("4", key -> new Integer(4));//key不存在执行函数存入
+        System.out.println(integer);
+        System.out.println(integer1);
+        System.out.println(map.toString());
+    }
+
+    public static void testMap2() {
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("1", 1);
+        map.put("2", 2);
+        map.put("3", 3);
+        //只对map中存在的key对应的value进行操作
+        Integer integer = map.computeIfPresent("3", (k, v) -> v + 1);
+        Integer integer1 = map.computeIfPresent("4", (k, v) -> {
+            if (v == null) {
+                return 0;
+            }
+            return v + 1;
+        });
+        System.out.println(integer);
+        System.out.println(integer1);
+        System.out.println(map.toString());
+    }
+
+    public static void main(String[] args) {
+//        testStream();
+//        testMap1();
+        testMap2();
+    }
+}
