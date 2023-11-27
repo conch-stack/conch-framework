acf453a15c7e82add2bf691a25a7e8b1d42e8c80
zjz6b6 <407091038@qq.com>
2023-11-27 14:09:16 +0800

    chore: append

--- conch-sample/src/main/java/com/nabob/conch/sample/hgroup/test/AbstractTestHandler.java
+++ conch-sample/src/main/java/com/nabob/conch/sample/hgroup/test/AbstractTestHandler.java
@@ -1,5 +1,15 @@
-package com.nabob.conch.sample.hgroup.test;/**
+package com.nabob.conch.sample.hgroup.test;
+
+/**
  * @author Adam
  * @since 2023/11/24
- */public class AbstractTestHandler {
+ */
+public abstract class AbstractTestHandler implements TestHandler{
+    @Override
+    public void test() {
+        System.out.println("==============================AbstractTestHandler==============================");
+        doTest();
+    }
+
+    public abstract void doTest();
 }
