3b5837ea1bffc15ad86e33b12300b61109ed1e3b
zjz6b6 <407091038@qq.com>
2024-11-27 21:35:52 +0800

    add

--- conch-sample/src/main/java/com/nabob/conch/sample/byteBuddy/test1/Test.java
+++ conch-sample/src/main/java/com/nabob/conch/sample/byteBuddy/test1/Test.java
@@ -8,18 +8,25 @@ import net.bytebuddy.matcher.ElementMatchers;
 
 import java.io.Serializable;
 
+import static net.bytebuddy.matcher.ElementMatchers.isDeclaredBy;
+import static net.bytebuddy.matcher.ElementMatchers.named;
+import static net.bytebuddy.matcher.ElementMatchers.takesArguments;
+
 /**
  * @author Adam
  * @since 2024/11/27
  */
 public class Test {
 
+    public Test() throws InstantiationException, IllegalAccessException {
+    }
+
     public static void main(String[] args) throws InstantiationException, IllegalAccessException {
         Class<?> dynamicType = new ByteBuddy()
                 .subclass(Object.class)
                 .implement(Serializable.class)
                 .name("HelloWorld")
-                .method(ElementMatchers.named("toString"))
+                .method(named("toString"))
 //                .intercept(FixedValue.value("Hello World!"))
                 .intercept(MethodDelegation.to(new LogInterceptor()))
                 .make()
@@ -32,4 +39,23 @@ public class Test {
 //                .and(ElementMatchers.returns(String.class))
 //                .and(ElementMatchers.takesArguments(1));
     }
+
+    class Foo {
+        public String bar() { return null; }
+        public String foo() { return null; }
+        public String foo(Object o) { return null; }
+    }
+
+    Foo dynamicFoo = new ByteBuddy()
+            .subclass(Foo.class)
+            // 匹配由Foo.class声明的方法
+            .method(isDeclaredBy(Foo.class)).intercept(FixedValue.value("One!"))
+            // 匹配名为foo的方法
+            .method(named("foo")).intercept(FixedValue.value("Two!"))
+            // 匹配名为foo，入参数量为1的方法
+            .method(named("foo").and(takesArguments(1))).intercept(FixedValue.value("Three!"))
+            .make()
+            .load(getClass().getClassLoader())
+            .getLoaded()
+            .newInstance();
 }
